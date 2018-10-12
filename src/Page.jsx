1e0746d76224a9b33e5946077c9ba89d15dcbe2b
Michael Pu <michael.pu123@gmail.com>
2018-10-11 22:53:00 -0400

    Add bootstrap, config

---
+++ src/Page.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import horoscopes from './horoscopes.json';
+
+const REQ_URL = 'https://horoscope-api.herokuapp.com';
+
+export class Page extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null
+        };
+    }
+
+    componentDidMount() {
+        let allSignsElement = [];
+        for (const sign of horoscopes['signs']) {
+            axios.get(REQ_URL + '/horoscope/today/' + sign['Sign'])
+                .then((res) => {
+                    console.log(res);
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='container mt-5'>
+                    <h1>Horroscopes</h1>
+                    {this.state.data ? 'Loading...' : this.state.data}
+                    {JSON.stringify(horoscopes)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Page;
